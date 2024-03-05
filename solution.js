92958a57a190505d2ea4113655e762e8c481a042
svetlilaloli <svetlilaloli@gmail.com>
2024-03-05 06:41:41 +0000

    Implemented Guest Details form functionality

--- solution.js
+++ solution.js
@@ -22,6 +22,7 @@ function cleanData(e) {
     changeContent('search-form-content');
 }
 
+changeContent('search-form-content');
 document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
 
 function searchFormData(e) {
@@ -39,6 +40,7 @@ function searchFormData(e) {
         changeContent('search-result-form-content');
     }
 }
+
 document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
 
 function fillSearchForm(e) {
@@ -48,7 +50,6 @@ function fillSearchForm(e) {
     document.querySelector('#check-out').value = reservation.endDate;
     document.querySelector('#people').value = reservation.guestsCount;
 }
-
 
 document.querySelectorAll('.room-type').forEach(room => {
     room.addEventListener("click", (e) => selectRoomType(e))
@@ -75,5 +76,42 @@ function findRoom(e) {
     reservation.roomType = roomInfo;
     console.log(reservation);
     changeContent('guest-details-form-content');
-
 }
+
+document.querySelector('#guest-details-back-btn').addEventListener('click', (e) => fillRoomForm(e));
+
+function fillRoomForm(e) {
+    e.preventDefault();
+    changeContent('search-result-form-content');
+}
+
+document.querySelector('#guest-details-next-btn').addEventListener('click', (e) => getPersonalData(e));
+
+function getPersonalData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement.parentElement;
+
+    const name = data.querySelector('#name').value;
+    const phone = data.querySelector('#phone-number').value;
+    const email = data.querySelector('#email').value;
+
+    if (name != '' && phone != '' && email != '') {
+        reservation.name = name;
+        reservation.phone = phone;
+        reservation.email = email;
+        console.log(reservation);
+        changeContent('confirm-reservation-content');
+        fillConfirmReservationData(reservation);
+    }
+}
+
+function fillConfirmReservationData(customReservation) {
+    document.querySelector('.confirm-reservation #guest-name').textContent = `Name: ${customReservation.name}`;
+    document.querySelector('.confirm-reservation #guest-phone').textContent = `Phone Number: ${customReservation.phone}`;
+    document.querySelector('.confirm-reservation #guest-email').textContent = `Email: ${customReservation.email}`;
+    document.querySelector('.confirm-reservation #guest-room-type').textContent = `Room Type: ${customReservation.roomType}`;
+    document.querySelector('.confirm-reservation #guest-data-in').textContent = `Date-in: ${customReservation.startDate}`;
+    document.querySelector('.confirm-reservation #guest-data-out').textContent = `Date-out: ${customReservation.endDate}`;
+}
+
+changeContent('guest-details-form-content');
